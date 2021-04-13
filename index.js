be984ff6d531a2594603ea3234d42593da09dee4
Gidon <gidonkatten@gmail.com>
2021-04-13 16:15:17 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.static('public')); /* this line tells Express to use the public folder as our static folder from which we can serve static files*/
+
+app.get('/approval_program', function(req, res) {
+  res.sendFile('./teal/approval_program.teal', { root: __dirname });
+});
+
+app.get('/clear_program', function(req, res) {
+  res.sendFile('./teal/clear.teal', { root: __dirname });
+});
+
+const PORT = process.env.PORT || 5000; // use either the host env var port (PORT) provided by Heroku or the local port (9000) on your machine
+app.listen(PORT, () => {
+  console.log("Server has started on port", PORT)
+});
